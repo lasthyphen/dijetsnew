313dcafbbd4efe65b7b5b6fb3c84e7fef9ca835f
Saleem Farid <davidhill0173@gmail.com>
2024-11-11 19:07:30 +0000

    frv02

---
+++ _next/static/chunks/pages/council-a6c92cda00c82e8f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[5518],{3152:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/council",function(){return _(32915)}])},32915:function(n,u,_){"use strict";_.r(u);var t=_(15700);u.default=t.default}},function(n){n.O(0,[995,1706,7493,6594,5700,9774,2888,179],(function(){return u=3152,n(n.s=u);var u}));var u=n.O();_N_E=u}]);
---
+++ _next/static/chunks/pages/djt/Djt-390a50ebb4aad09d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[218],{50808:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/djt/Djt",function(){return u(62277)}])}},function(n){n.O(0,[995,1706,7493,6594,2277,9774,2888,179],(function(){return _=50808,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
